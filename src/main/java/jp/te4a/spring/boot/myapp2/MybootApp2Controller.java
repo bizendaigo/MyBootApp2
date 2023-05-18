7b34f9f6501071fb9a1743f71abe9641e9c34859
bizendaigo <200734@jc-21.jp>
2023-05-18 12:30:37 +0900

    課題２コミットメッセージ

---
+++ src/main/java/jp/te4a/spring/boot/myapp2/MybootApp2Controller.java
@@ -0,0 +1,17 @@
+package jp.te4a.spring.boot.myapp2;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+* Hello world!
+*
+*/
+@RestController
+public class MybootApp2Controller
+{
+	@RequestMapping("/taro")
+	public String taro() {
+		return "Taro desu!";
+	}
+}
